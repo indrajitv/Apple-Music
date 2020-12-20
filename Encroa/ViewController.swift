363fb2c0d403861ff9e356a0af8ad015b5879599
Indrajit Chavda <indrajit@sculptsoft.com>
2020-12-20 14:01:28 +0530

    Initial Commit

---
+++ Encroa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Encroa
+//
+//  Created by Indrajit Chavda on 20/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
